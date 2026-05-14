fc0444ad9780df7a7569fec96352a64ec15ee050
IMO <180146096+IMOitself@users.noreply.github.com>
2026-05-14 16:35:30 +0800

    repo: add screenshots & fin screenshot code

--- screenshot-code.js
+++ screenshot-code.js
@@ -1,9 +1,9 @@
 // use this on browser's console of https://learn.cylabacademy.org/library page
-// TODO: find the thing we need to screenshot hehe
+// ensure 'Progress Tracker' dropdown is shown
 let s = document.createElement('script');
 s.src = 'https://cdnjs.cloudflare.com/ajax/libs/html-to-image/1.11.11/html-to-image.min.js';
 document.head.appendChild(s);
-s.onload = () => htmlToImage.toPng(document.querySelector('.CHANGEME'), { backgroundColor: '#ffffff' }).then(d => {
+s.onload = () => htmlToImage.toPng(document.querySelector('#main-content > div > div > div:nth-child(1) > div.disclosure > div')).then(d => {
     let a = document.createElement('a');
     a.download = 'screenshot.png';
     a.href = d;
