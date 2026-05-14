c07d597c79ade12252ef0e944ce1fbf4941b872a
IMO <180146096+IMOitself@users.noreply.github.com>
2026-05-14 15:11:18 +0800

    freshly made files;
    referenced codes from https://github.com/IMOitself/a1sberg-ctf-walkthrough

---
+++ screenshot-code.js
@@ -0,0 +1,11 @@
+// use this on browser's console of https://learn.cylabacademy.org/library page
+// TODO: find the thing we need to screenshot hehe
+let s = document.createElement('script');
+s.src = 'https://cdnjs.cloudflare.com/ajax/libs/html-to-image/1.11.11/html-to-image.min.js';
+document.head.appendChild(s);
+s.onload = () => htmlToImage.toPng(document.querySelector('.CHANGEME'), { backgroundColor: '#ffffff' }).then(d => {
+    let a = document.createElement('a');
+    a.download = 'screenshot.png';
+    a.href = d;
+    a.click();
+});
